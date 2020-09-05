2dd9ab42a829e7a4d94616fbc2ff672f1895f5e7
HarishMurugancoolz <48434581+HarishMurugancoolz@users.noreply.github.com>
2020-09-05 18:07:55 +0530

    Update anagram.cpp

--- anagram.cpp
+++ anagram.cpp
@@ -12,13 +12,25 @@ bool isInsideAtoZ(const char CH){
     return false;
 }
 void populateWordsForaz(int alphabets[],const string& word){
-    for(int i_word=0;i_word<word.length();i_word++){
+    for(unsigned int i_word=0;i_word<word.length();i_word++){
         if(isInsideatoz(word[i_word]))
             alphabets[word[i_word]-97]++;
     }
 }
 void populateWordsForAZ(int alphabets[],const string& word){
-    for(int i_word=0;i_word<word.length();i_word++){
+    for(unsigned int i_word=0;i_word<word.length();i_word++){
+            if(isInsideAtoZ(word[i_word]))
+            alphabets[word[i_word]-65]++;
+    }
+}
+void dePopulateWordsForaz(int alphabets[],const string& word){
+    for(unsigned int i_word=0;i_word<word.length();i_word++){
+        if(isInsideatoz(word[i_word]))
+            alphabets[word[i_word]-97]++;
+    }
+}
+void dePopulateWordsForAZ(int alphabets[],const string& word){
+    for(unsigned int i_word=0;i_word<word.length();i_word++){
             if(isInsideAtoZ(word[i_word]))
             alphabets[word[i_word]-65]++;
     }
@@ -28,17 +40,17 @@ bool Anagram::WordPairIsAnagram(const string& word1, const string& word2) {
     int total_alphabets=26;
     populateWordsForaz(alphabets,word1);
     populateWordsForAZ(alphabets,word1);
-    populateWordsForaz(alphabets,word2);
-    populateWordsForAZ(alphabets,word2);
-    for(int i_alphabets=0;i_alphabets<total_alphabets;i_alphabets++){
-        if((alphabets[i_alphabets]%2)!=0)
+    dePopulateWordsForaz(alphabets,word2);
+    dePopulateWordsForAZ(alphabets,word2);
+    for(unsigned int i_alphabets=0;i_alphabets<total_alphabets;i_alphabets++){
+        if((alphabets[i_alphabets]!=0)//all elements should br zero for anagram
             return false;
     }
     return true;
 }
 vector<string> Anagram::SelectAnagrams(const string& word,const vector<string>& candidates) {
     vector<string> anagramOfWords;
-    for(int i_candidate=0;i_candidate<candidates.size();i_candidate++){
+    for(unsigned int i_candidate=0;i_candidate<candidates.size();i_candidate++){
         if(WordPairIsAnagram(word,candidates[i_candidate]))
                 anagramOfWords.push_back(candidates[i_candidate]);
     }
