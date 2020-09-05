a72b220a53eb5dd8d62c5198e6d07364e6b20971
HarishMurugancoolz <48434581+HarishMurugancoolz@users.noreply.github.com>
2020-09-05 18:22:50 +0530

    Update anagram.cpp

--- anagram.cpp
+++ anagram.cpp
@@ -26,18 +26,18 @@ void populateWordsForAZ(int alphabets[],const string& word){
 void dePopulateWordsForaz(int alphabets[],const string& word){
     for(unsigned int i_word=0;i_word<word.length();i_word++){
         if(isInsideatoz(word[i_word]))
-            alphabets[word[i_word]-97]++;
+            --alphabets[word[i_word]-97];
     }
 }
 void dePopulateWordsForAZ(int alphabets[],const string& word){
     for(unsigned int i_word=0;i_word<word.length();i_word++){
             if(isInsideAtoZ(word[i_word]))
-            alphabets[word[i_word]-65]++;
+            alphabets[word[i_word]-65]--;
     }
 }
 bool Anagram::WordPairIsAnagram(const string& word1, const string& word2) {
     int alphabets[26]={0};
-    int total_alphabets=26;
+    unsigned int total_alphabets=26;
     populateWordsForaz(alphabets,word1);
     populateWordsForAZ(alphabets,word1);
     dePopulateWordsForaz(alphabets,word2);
@@ -50,7 +50,7 @@ bool Anagram::WordPairIsAnagram(const string& word1, const string& word2) {
 }
 vector<string> Anagram::SelectAnagrams(const string& word,const vector<string>& candidates) {
     vector<string> anagramOfWords;
-    for(int i_candidate=0;i_candidate<candidates.size();i_candidate++){
+    for(signed int i_candidate=0;i_candidate<candidates.size();i_candidate++){
         if(WordPairIsAnagram(word,candidates[i_candidate]))
                 anagramOfWords.push_back(candidates[i_candidate]);
     }
